1efb054de856bbd057aec93758199cf202c8ddd6
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2025-03-01 17:21:54 +0530

    cors

--- Server.js
+++ Server.js
@@ -9,17 +9,13 @@ const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 
 // Sample Route
-const allowedOrigins = ["https://navpran.vercel.app"];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+const corsOptions = {
+  origin: ['https://navpran.vercel.app'],
+  methods: ['GET', 'POST', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Allow credentials (cookies, authorization headers)
+};
+app.use(cors(corsOptions));
 
 app.use(express.json()); // Replace bodyParser with Express built-in
 app.use(express.urlencoded({ extended: true }));
